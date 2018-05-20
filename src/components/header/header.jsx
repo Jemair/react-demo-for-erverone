58511596197f44131c17d776dbaeacb589fcf37f
J <wang940110@163.com>
2018-05-20 19:56:55 +0800

    1.0 添加一个header和顶部菜单

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,57 @@
 import React, { PureComponent } from 'react'
-import { NavBar } from 'antd-mobile'
+import { NavBar, Menu } from 'antd-mobile'
 import s from './header.scss'
 
+const initData = [{
+  label: '菜单1号',
+  value: 1,
+}, {
+  label: '菜单2号',
+  value: 2,
+}, {
+  label: '菜单3号',
+  value: 3,
+}]
+
 export default class Header extends PureComponent {
+  state = {
+    showMenu: false,
+    chosenValue: [1],
+  }
+
+  handleLeftClick = e => {
+    this.setState({ showMenu: true })
+    e.nativeEvent.stopImmediatePropagation()
+    document.addEventListener('click', () => {
+      this.setState({ showMenu: false })
+    })
+  }
+
+  handleMenuChange = value => {
+    this.setState({ chosenValue: value })
+  }
+
   render() {
+    const { showMenu, chosenValue } = this.state
+
     return (
-      <div className={s.div}>
-        <NavBar />
+      <div className={s.header}>
+        <NavBar
+          leftContent={'Menu'}
+          onLeftClick={this.handleLeftClick}
+        >
+          一看就会的React demo
+        </NavBar>
+        { showMenu &&
+          <Menu
+            className={s.menu}
+            onChange={this.handleMenuChange}
+            data={initData}
+            value={chosenValue}
+            level={1}
+            height={document.documentElement.clientHeight * 0.3}
+          />
+        }
       </div>
     )
   }
